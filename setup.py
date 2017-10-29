7a0e2886701d20c786f1264b12878332f48224a6
David Tucker <david@tucker.name>
2017-10-29 00:02:59 -0700

    Mark the project stable

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
     ],
 )
