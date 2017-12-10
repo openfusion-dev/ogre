d3d03c325f9dca5bb11dac313577c4b2411df288
David Tucker <david@tucker.name>
2017-12-10 12:21:32 -0800

    Drop support for Python 3.3

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     packages=find_packages(exclude=['docs']),
     include_package_data=True,
     test_suite="ogre.test",
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,<4',
     install_requires=[
         'future ~= 0.16.0',
         'mock ~= 1.0.1',
@@ -37,7 +38,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
