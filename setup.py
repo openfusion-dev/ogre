94057972890169b80c1889423d97dac9c36aa099
David Tucker <david@tucker.name>
2023-07-15 17:12:07 -0700

    Test with Python 3.10

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Development Status :: 5 - Production/Stable",
     ],
 )
